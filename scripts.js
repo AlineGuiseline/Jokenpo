3e1ee8aa3e909d517f93995939750190e28ca852
Aline Guiseline <alineguiseline@gmail.com>
2024-09-23 21:56:15 -0300

    add ENUM on script.js

--- scripts.js
+++ scripts.js
@@ -5,12 +5,19 @@ const machineScore = document.querySelector("#machine-score");
 let humanScoreNumber = 0
 let machineScoreNumber = 0
 
+// ENUM
+const GAME_OPTIONS = {
+  ROCK: 'pedra',
+  PAPER: 'papel',
+  SCISSORS: 'tesoura'
+}
+
 const playHuman = (humanChoice) => {
   playTheGame(humanChoice, playMachine());
 };
 
 const playMachine = () => {
-  const options = ["pedra", "papel", "tesoura"];
+  const options = [GAME_OPTIONS.ROCK, GAME_OPTIONS.PAPER, GAME_OPTIONS.SCISSORS];
   const randomIndex = Math.floor(Math.random() * 3);
 
   return options[randomIndex];
@@ -20,9 +27,9 @@ const playTheGame = (human, machine) => {
   if (human === machine) {
     result.innerHTML = "Deu empate";
   } else if (
-    (human === "papel" && machine === "pedra") ||
-    (human === "pedra" && machine === "tesoura") ||
-    (human === "tesoura" && machine === "papel")
+    (human === GAME_OPTIONS.PAPER && machine === GAME_OPTIONS.ROCK) ||
+    (human === GAME_OPTIONS.ROCK && machine === GAME_OPTIONS.SCISSORS) ||
+    (human === GAME_OPTIONS.SCISSORS && machine === GAME_OPTIONS.PAPER)
   ) {
     humanScoreNumber++
     humanScore.innerHTML = humanScoreNumber
